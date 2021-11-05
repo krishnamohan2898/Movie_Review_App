aa2499b385985c2903f293956d8b4adbc3360f81
krishnamohan2898 <92075817+krishnamohan2898@users.noreply.github.com>
2021-11-05 22:58:01 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -16,11 +16,11 @@ function App() {
       <Typography variant="h2" style={{padding:10,fontFamily:"fantasy",color:"blueviolet"}} align="center">MOVIE APP</Typography>
       <Grid container style={{padding:10}}> 
      
-       {/* <Grid item xs={12} sm={7} md={7} style = {{padding:10}}> <Display /></Grid>
+       <Grid item xs={12} sm={7} md={7} style = {{padding:10}}> <Display /></Grid>
         <Grid item xs={12} sm={5} md={5} style = {{padding:10}}> <Searchtodo/></Grid>
-       <Grid item xs={12} sm={7} md={7} style = {{padding:10}}><Addmovi/> </Grid> */}
+       <Grid item xs={12} sm={7} md={7} style = {{padding:10}}><Addmovi/> </Grid>
 
-       <Deletemovi/>
+       
 
       </Grid> 
 
